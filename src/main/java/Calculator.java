f835f6b8b44654ea11116fdec4895a5ff404c748
Julien Grosthor <julien.grosthor@le-campus-numerique.fr>
2024-08-28 13:14:51 +0200

    commit  début d'apremÃ

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -9,6 +9,9 @@ public class Calculator {
         return a * b;
     }
     public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Divide by zero ERROR");
+        }
         return a / b;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
@@ -9,20 +11,33 @@ public class CalculatorTest {
     @RepeatedTest(5)
     public void testAdd() {
         float result = calculator.add(3, 3.5f);
+        assertEquals(6.5, result);
+
     }
 
     @Test
     public void testSubtract() {
         int result = calculator.subtract(5, 3);
+        assertEquals(2, result);
     }
 
     @Test
     public void testMultiply() {
         int result = calculator.multiply(4, 3);
+        assertEquals(12, result);
     }
 
     @Test
     public void testDivide() {
         int result = calculator.divide(10, 2);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testDivideByZero() {
+        Calculator calculator = new Calculator();
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.divide(10, 0);
+        });
     }
 }
