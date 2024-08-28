a8d3283b43a6f928e05531f09cefaf95bbd9b422
Julien Grosthor <julien.grosthor@le-campus-numerique.fr>
2024-08-28 11:57:59 +0200

    new tests made

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,30 +1,28 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
     private Calculator calculator= new Calculator();
 
-    @Test
+    @RepeatedTest(5)
     public void testAdd() {
-//        Calculator calculator = new Calculator();
-        int result = calculator.add(3, 3);
+        float result = calculator.add(3, 3.5f);
     }
 
     @Test
     public void testSubtract() {
-//        Calculator calculator = new Calculator();
         int result = calculator.subtract(5, 3);
     }
 
     @Test
     public void testMultiply() {
-//        Calculator calculator = new Calculator();
         int result = calculator.multiply(4, 3);
     }
 
     @Test
     public void testDivide() {
-//        Calculator calculator = new Calculator();
         int result = calculator.divide(10, 2);
     }
 }
